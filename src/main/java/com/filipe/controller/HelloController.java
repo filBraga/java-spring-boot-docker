f86d8d8637f92b729aa9b41b60e337414f3db6aa
filipebraga <fbragasoftware@gmail.com>
2024-12-18 19:51:00 -0300

    move hello

--- src/main/java/com/filipe/controller/HelloController.java
+++ src/main/java/com/filipe/controller/HelloController.java
@@ -1,5 +1,5 @@
 
-package com.filipe;
+package com.filipe.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World 2!";
     }
